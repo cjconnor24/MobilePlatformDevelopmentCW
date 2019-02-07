e30f1e7d2d24b53ca8b1c539b085d4c44648c2db
Chris Connor <cjconnor24@connordesign.com>
2019-02-07 22:52:17 +0000

    Adding new classes to store earthquake data

---
+++ app/src/main/java/uk/co/chrisconnor/mpdcw/Earthquake.java
@@ -0,0 +1,20 @@
+package uk.co.chrisconnor.mpdcw;
+
+public class Earthquake {
+
+    // GENERAL DATA
+    private String description;
+    private Location location;
+    private String dateTime;
+    private String publishedDateTime;
+
+    // STAT DATA
+    private int depth;
+    private double magnitude;
+
+    // META
+    private String category;
+    private String link;
+
+
+}
---
+++ app/src/main/java/uk/co/chrisconnor/mpdcw/Location.java
@@ -0,0 +1,41 @@
+package uk.co.chrisconnor.mpdcw;
+
+/**
+ * Represents Earthquake Location
+ */
+public class Location {
+
+    private String name;
+    private double lat;
+    private double lon;
+
+    public Location(String name, double lat, double lon) {
+        this.name = name;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+}
