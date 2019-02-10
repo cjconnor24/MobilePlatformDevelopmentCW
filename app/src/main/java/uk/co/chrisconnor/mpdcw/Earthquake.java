e91688d2e2f1b78a8f8571ec349057535e3baf1c
Chris Connor <cjconnor24@connordesign.com>
2019-02-10 22:24:08 +0000

    fixing getters and setters:

--- app/src/main/java/uk/co/chrisconnor/mpdcw/Earthquake.java
+++ app/src/main/java/uk/co/chrisconnor/mpdcw/Earthquake.java
@@ -16,5 +16,67 @@ public class Earthquake {
     private String category;
     private String link;
 
+    public String getDescription() {
+        return description;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getPublishedDateTime() {
+        return publishedDateTime;
+    }
+
+    public void setPublishedDateTime(String publishedDateTime) {
+        this.publishedDateTime = publishedDateTime;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    public double getMagnitude() {
+        return magnitude;
+    }
+
+    public void setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 }
---
+++ app/src/main/java/uk/co/chrisconnor/mpdcw/XMLParser.java
@@ -0,0 +1,4 @@
+package uk.co.chrisconnor.mpdcw;
+
+public class XMLParser {
+}
