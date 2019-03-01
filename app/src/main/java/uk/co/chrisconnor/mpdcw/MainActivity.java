6a8b2103ff9ad99d26449a51987dbf40f48674e5
Chris Connor <chris@chrisconnor.co.uk>
2019-03-01 09:52:21 +0000

    Refactored most of the ASYNC stuff now into DownloadData class

--- app/src/main/java/uk/co/chrisconnor/mpdcw/MainActivity.java
+++ app/src/main/java/uk/co/chrisconnor/mpdcw/MainActivity.java
@@ -74,8 +74,27 @@ public class MainActivity extends AppCompatActivity implements DownloadData.OnDo
     public void onDownloadComplete(String data, DownloadStatus status) {
 
         if (status == DownloadStatus.OK) {
-            Log.d(TAG, "onDownloadComplete: DATA IS" + data);
-            Log.d(TAG, "onDownloadComplete: STATUS IS" + status.toString());
+
+            Log.d(TAG, "onDownloadComplete: STATUS IS " + status.toString());
+
+            ParseEarthquakes parseEarthquakes = new ParseEarthquakes();
+            parseEarthquakes.parse(data);
+            earthquakes = parseEarthquakes.getEarthquakes();
+            Log.d(TAG, "onDownloadComplete: RETURNED " + earthquakes.size() + " earthquakes");
+
+            // CREATE AN INSTANCE OF THE NEW CUSTOM FEED ADAPTER AND SET THE SOURCE
+            EarthquakeListAdapter earthquakeListAdapter = new EarthquakeListAdapter(MainActivity.this, R.layout.list_earthquake, parseEarthquakes.getEarthquakes());
+            earthquakeList.setAdapter(earthquakeListAdapter);
+
+            // CREATE A DUMMY TOAST ITEM WHEN SOMEONE CLICKS
+            earthquakeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Toast.makeText(MainActivity.this, "The depth for Earthquake " + position + " was " + earthquakes.get(position).getDepth() + "km", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
         } else {
             Log.e(TAG, "onDownloadComplete: Something went wrong" + status.toString());
         }
@@ -89,109 +108,5 @@ public class MainActivity extends AppCompatActivity implements DownloadData.OnDo
         // TODO: FIND A WAY TO PUT OBJECTS INTO THE BUNDLE
         super.onSaveInstanceState(outState);
     }
-
-    // DOWNLOAD THE XML DATA FROM THE PASSED URL
-//    private void downloadUrl(String feedUrl) {
-//
-//        Log.d(TAG, "downloadUrl: starting Async Task");
-//        DownloadData downloadData = new DownloadData();
-//        downloadData.execute(feedUrl);
-//        Log.d(TAG, "downloadUrl: done");
-//
-//    }
-
-
-    // ASYNC CLASS TASK TO DOWNLOAD DATA ON SEPARATE THREAD
-//    private class DownloadData extends AsyncTask<String, Void, String> {
-//
-//        private static final String TAG = "DownloadData";
-//
-//        @Override
-//        protected void onPostExecute(String s) {
-//
-//            super.onPostExecute(s);
-//
-//            // GET THE XML DATA PARSED TO OBJECTS
-//            final ParseEarthquakes parseEarthquakes = new ParseEarthquakes();
-//            parseEarthquakes.parse(s);
-//
-//            earthquakes = parseEarthquakes.getEarthquakes();
-//
-//            // CREATE AN INSTANCE OF THE NEW CUSTOM FEED ADAPTER AND SET THE SOURCE
-//            EarthquakeListAdapter earthquakeListAdapter = new EarthquakeListAdapter(MainActivity.this, R.layout.list_earthquake, parseEarthquakes.getEarthquakes());
-//            earthquakeList.setAdapter(earthquakeListAdapter);
-//
-//            // CREATE A DUMMY TOAST ITEM WHEN SOMEONE CLICKS
-//            earthquakeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//                @Override
-//                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                    Toast.makeText(MainActivity.this, "The depth for Earthquake " + position + " was " + parseEarthquakes.getEarthquakes().get(position).getDepth() + "km", Toast.LENGTH_SHORT).show();
-//                }
-//            });
-//
-//        }
-//
-//        @Override
-//        protected String doInBackground(String... strings) {
-//            Log.d(TAG, "doInBackground: starts with " + strings[0]);
-//            String rssFeed = downloadXML(strings[0]);
-//            if (rssFeed == null) {
-//                Log.e(TAG, "doInBackground: Error downloading");
-//            }
-//            return rssFeed;
-//        }
-//
-//        /**
-//         * Open up the connection
-//         *
-//         * @param urlPath
-//         * @return
-//         */
-//        private String downloadXML(String urlPath) {
-//            StringBuilder xmlResult = new StringBuilder();
-//
-//            try {
-//                URL url = new URL(urlPath);
-//                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//                int response = connection.getResponseCode();
-//
-//                Log.d(TAG, "downloadXML: ** Downloaded the XML DATA from URL ** " + new Date().toString());
-//                Log.d(TAG, "downloadXML: The response code was " + response);
-//
-//
-//                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//                int charsRead;
-//                char[] inputBuffer = new char[500]; // CREATE A BUFFER
-//
-//                // START TO LOOP
-//                while (true) {
-//
-//                    // ADD TO BUFFER
-//                    charsRead = reader.read(inputBuffer);
-//                    if (charsRead < 0) {
-//                        break; // BREAK IF COMPLETE
-//                    }
-//                    if (charsRead > 0) {
-//                        xmlResult.append(String.copyValueOf(inputBuffer, 0, charsRead));
-//                    }
-//                }
-//                reader.close();
-//
-//                return xmlResult.toString();
-//
-//            } catch (MalformedURLException e) {
-//                Log.e(TAG, "downloadXML: Invalid URL" + e.getMessage());
-//            } catch (IOException e) {
-//                Log.e(TAG, "downloadXML: IO Exception reading data:" + e.getMessage());
-//            } catch (SecurityException e) {
-//                Log.e(TAG, "downloadXML: cannot access internet: Needs permission? " + e.getMessage());
-//            }
-//
-//
-//            return null;
-//
-//        }
-//
-//    }
 
 }
