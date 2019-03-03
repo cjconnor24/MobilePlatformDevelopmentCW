26ef71cd40a2f36b1320aab53ae2d34ff3524f18
Chris Connor <chris@chrisconnor.co.uk>
2019-03-03 13:22:53 +0000

    Adding a circle drawable and updating the adapter to change the colour depending on the magnitude

--- app/src/main/java/uk/co/chrisconnor/mpdcw/EarthquakeListAdapter.java
+++ app/src/main/java/uk/co/chrisconnor/mpdcw/EarthquakeListAdapter.java
@@ -1,6 +1,10 @@
 package uk.co.chrisconnor.mpdcw;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -50,7 +54,58 @@ public class EarthquakeListAdapter extends ArrayAdapter {
         viewHolder.eDate.setText(PrettyDate.getTimeAgo(currentEarthquake.getDate()));
         viewHolder.eMagnitude.setText(String.valueOf(currentEarthquake.getMagnitude()));
 
+        // SET THE BACKGROUND COLOR OF MAGNITUDE
+        Drawable d = viewHolder.eMagnitude.getBackground();
+        d.setColorFilter(getColor(currentEarthquake.getMagnitude()+2), PorterDuff.Mode.MULTIPLY);
+
+
         return convertView;
+    }
+
+    private int getColor(double magnitude){
+
+        int color;
+        Double m = Math.abs(magnitude);
+        int mag = m.intValue();
+
+        switch(mag){
+            case 0:
+                color = Color.parseColor("#99FF99");
+                break;
+            case 1:
+                color = Color.parseColor("#BBFF77");
+                break;
+            case 2:
+                color = Color.parseColor("#DDFF55");
+                break;
+            case 3:
+                color = Color.parseColor("#FFFF33");
+                break;
+            case 4:
+                color = Color.parseColor("#FBD33D");
+                break;
+            case 5:
+                color = Color.parseColor("#F8A746");
+                break;
+            case 6:
+                color = Color.parseColor("#F47B50");
+                break;
+            case 7:
+                color = Color.parseColor("#E35F44");
+                break;
+            case 8:
+                color = Color.parseColor("#D24339");
+                break;
+            case 9:
+                color = Color.parseColor("#C1272D");
+                break;
+            default:
+                color = Color.parseColor("#99FF99");
+                break;
+        }
+
+        return color;
+
     }
 
     // THIS CREATES A RE-USABLE VIEW
