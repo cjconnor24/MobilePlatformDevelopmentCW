61f87854e26b4290d88fc85cbf7752bddfb30e84
Chris Connor <chris@chrisconnor.co.uk>
2019-04-02 23:14:20 +0100

    Updating menus etc.

--- app/src/main/java/uk/co/chrisconnor/mpdcw/MainActivity.java
+++ app/src/main/java/uk/co/chrisconnor/mpdcw/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -13,8 +14,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
