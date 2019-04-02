768ce6dac59277fb4a0b87943399acd37a411d8c
Chris Connor <chris@chrisconnor.co.uk>
2019-04-02 12:54:51 +0100

    Add base files

--- app/src/main/java/uk/co/chrisconnor/mpdcw/XEarthquakeDetailFragment.java
+++ app/src/main/java/uk/co/chrisconnor/mpdcw/XEarthquakeDetailFragment.java
@@ -104,11 +104,11 @@ public class XEarthquakeDetailFragment extends Fragment {
 //    @Override
 //    public void onAttach(Context context) {
 //        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
+//        if (context instanceof OnDashboardFragmentInteractionListener) {
+//            mListener = (OnDashboardFragmentInteractionListener) context;
 //        } else {
 //            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
+//                    + " must implement OnDashboardFragmentInteractionListener");
 //        }
 //    }
 
