1c1366e2d1c935e11efbf882b93f595faa4930ba
Chris Connor <chris@chrisconnor.co.uk>
2019-04-06 18:19:38 +0100

    Working on info window

--- app/src/main/java/uk/co/chrisconnor/mpdcw/EarthquakeInfoWindow.java
+++ app/src/main/java/uk/co/chrisconnor/mpdcw/EarthquakeInfoWindow.java
@@ -3,6 +3,7 @@ package uk.co.chrisconnor.mpdcw;
 import android.app.Activity;
 import android.content.Context;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
@@ -24,6 +25,11 @@ public class EarthquakeInfoWindow implements GoogleMap.InfoWindowAdapter {
     public View getInfoContents(Marker marker) {
 
         View v = ((Activity)mContext).getLayoutInflater().inflate(R.layout.map_info_window, null);
+
+        TextView lblDatetime = v.findViewById(R.id.lblDateTime);
+        TextView lblLocation = v.findViewById(R.id.lblLocation);
+        TextView lblMagnitude = v.findViewById(R.id.lblMagnitude);
+        TextView lblDepth = v.findViewById(R.id.lblDepth);
 
         return null;
     }
