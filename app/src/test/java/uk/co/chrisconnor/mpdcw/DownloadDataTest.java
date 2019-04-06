3096b0e98a00290d48aab90defd8ea84b487c9a2
Chris Connor <chris@chrisconnor.co.uk>
2019-04-06 18:20:14 +0100

    Testing download data

---
+++ app/src/test/java/uk/co/chrisconnor/mpdcw/DownloadDataTest.java
@@ -0,0 +1,19 @@
+package uk.co.chrisconnor.mpdcw;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DownloadDataTest {
+
+    @Test
+    public void onPostExecute() {
+
+
+
+    }
+
+    @Test
+    public void doInBackground() {
+    }
+}
